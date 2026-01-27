92579cecc7fe1ca62b3355f274e1c5289a3b9dfa
Giovanni Bonisoli <giovannibonisoli@yahoo.it>
2026-01-27 23:52:49 +0100

    Restore initial intraday collection process

--- fitbit_intraday.py
+++ fitbit_intraday.py
@@ -45,7 +45,7 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 # === BACKFILL CONFIGURATION ===
-BACKFILL_START_DATE = "2025-01-25"  # First day to collect (inclusive)
+BACKFILL_START_DATE = "2025-11-18"  # First day to collect (inclusive)
 SLEEP_ON_RATE_LIMIT = 600  # 10 minutes in seconds
 
 
@@ -87,10 +87,10 @@ def get_intraday_data(access_token, email_address, date_str, last_synch_date):
 
         
         metrics = [
-            # ('heart_rate', f"https://api.fitbit.com/1/user/-/activities/heart/date/{date_str}/1d/{detail_level}.json", 'activities-heart-intraday'),
-            # ('steps', f"https://api.fitbit.com/1/user/-/activities/steps/date/{date_str}/1d/{detail_level}.json", 'activities-steps-intraday'),
-            # ('calories', f"https://api.fitbit.com/1/user/-/activities/calories/date/{date_str}/1d/{detail_level}.json", 'activities-calories-intraday'),
-            # ('distance', f"https://api.fitbit.com/1/user/-/activities/distance/date/{date_str}/1d/{detail_level}.json", 'activities-distance-intraday'),
+            ('heart_rate', f"https://api.fitbit.com/1/user/-/activities/heart/date/{date_str}/1d/{detail_level}.json", 'activities-heart-intraday'),
+            ('steps', f"https://api.fitbit.com/1/user/-/activities/steps/date/{date_str}/1d/{detail_level}.json", 'activities-steps-intraday'),
+            ('calories', f"https://api.fitbit.com/1/user/-/activities/calories/date/{date_str}/1d/{detail_level}.json", 'activities-calories-intraday'),
+            ('distance', f"https://api.fitbit.com/1/user/-/activities/distance/date/{date_str}/1d/{detail_level}.json", 'activities-distance-intraday'),
             ('floors', f"https://api.fitbit.com/1/user/-/activities/floors/date/{date_str}/1d/{detail_level}.json", 'activities-floors-intraday'),
             ('elevation', f"https://api.fitbit.com/1/user/-/activities/elevation/date/{date_str}/1d/{detail_level}.json", 'activities-elevation-intraday'),
         ]
@@ -130,20 +130,20 @@ def get_intraday_data(access_token, email_address, date_str, last_synch_date):
         for timestamp in timestamps:
 
             values = data_points[timestamp]
-            # if not ('heart_rate' not in values and values['steps'] == 0 and values['distance'] == 0):
+            if not ('heart_rate' not in values and values['steps'] == 0 and values['distance'] == 0):
 
-            if db.check_intraday_timestamp(email_address['id'], timestamp):
-                # db.insert_intraday_metric(email_address['id'], timestamp, data_type='heart_rate', value=values.get('heart_rate', None))
-                # db.insert_intraday_metric(email_address['id'], timestamp, data_type='steps', value=values['steps'])
-                # db.insert_intraday_metric(email_address['id'], timestamp, data_type='distance', value=values['distance'])
-                # db.insert_intraday_metric(email_address['id'], timestamp, data_type='calories', value=values['calories'])
+            # if db.check_intraday_timestamp(email_address['id'], timestamp):
+                db.insert_intraday_metric(email_address['id'], timestamp, data_type='heart_rate', value=values.get('heart_rate', None))
+                db.insert_intraday_metric(email_address['id'], timestamp, data_type='steps', value=values['steps'])
+                db.insert_intraday_metric(email_address['id'], timestamp, data_type='distance', value=values['distance'])
+                db.insert_intraday_metric(email_address['id'], timestamp, data_type='calories', value=values['calories'])
 
                 db.insert_intraday_metric(email_address['id'], timestamp, data_type='floors', value=values['floors'])
                 db.insert_intraday_metric(email_address['id'], timestamp, data_type='elevation', value=values['elevation'])
             else:
                 print(f"Empty point or checkpoint reached for timestamp {timestamp}")
 
-            # db.update_intraday_checkpoint(email_address['id'], timestamp)
+            db.update_intraday_checkpoint(email_address['id'], timestamp)
 
 
         # Update checkpoint in database
@@ -198,6 +198,8 @@ def process_all_emails_continuous():
                 
             # Get checkpoint from database
             intraday_checkpoint = db.get_intraday_checkpoint(email_address['id'])
+
+            print(intraday_checkpoint)
   
             if intraday_checkpoint:
                 current_date = intraday_checkpoint + timedelta(minutes=1)
@@ -215,7 +217,6 @@ def process_all_emails_continuous():
                 continue
             
             try:
-
                 db = DatabaseManager()
                 if not db.connect():
                     continue
@@ -294,40 +295,34 @@ def process_all_emails_continuous():
             # Continue processing
             time.sleep(2)
 
-        db = DatabaseManager()
-    if not db.connect():
-        logger.error("Failed to connect to database")
-        return
-    
-    email_addresses = db.get_all_emails()
-    db.close()
+
 
 
 if __name__ == "__main__":
-    # os.makedirs("logs", exist_ok=True)
-    # logger.info("=== START OF FITBIT INTRADAY CONTINUOUS LOOP ===")
-    # logger.info(f"Python version: {sys.version}")
-    # logger.info(f"Working directory: {os.getcwd()}")
+    os.makedirs("logs", exist_ok=True)
+    logger.info("=== START OF FITBIT INTRADAY CONTINUOUS LOOP ===")
+    logger.info(f"Python version: {sys.version}")
+    logger.info(f"Working directory: {os.getcwd()}")
     
-    # try:
-    #     process_all_emails_continuous()
-    # except KeyboardInterrupt:
-    #     logger.info("\n=== STOPPED BY USER (Ctrl+C) ===")
-    # except Exception as e:
-    #     logger.error(f"Fatal error: {e}", exc_info=True)
+    try:
+        process_all_emails_continuous()
+    except KeyboardInterrupt:
+        logger.info("\n=== STOPPED BY USER (Ctrl+C) ===")
+    except Exception as e:
+        logger.error(f"Fatal error: {e}", exc_info=True)
     
-    db = DatabaseManager()
-    if db.connect():
+    # db = DatabaseManager()
+    # if db.connect():
 
-        current_date = datetime.strptime(BACKFILL_START_DATE, "%Y-%m-%d").date()
-        email_addresses = db.get_all_emails()
+    #     current_date = datetime.strptime(BACKFILL_START_DATE, "%Y-%m-%d").date()
+    #     email_addresses = db.get_all_emails()
 
-        while True:
+    #     while True:
         
-            for email_address in email_addresses:
-                last_synch_date = db.get_last_synch(email_address['id'])
-                access_token, refresh_token = db.get_email_tokens(email_address['id'])
-                date_str = current_date.strftime('%Y-%m-%d')
-                success, hit_rate_limit = get_intraday_data(access_token, email_address, date_str, last_synch_date)
+    #         for email_address in email_addresses:
+    #             last_synch_date = db.get_last_synch(email_address['id'])
+    #             access_token, refresh_token = db.get_email_tokens(email_address['id'])
+    #             date_str = current_date.strftime('%Y-%m-%d')
+    #             success, hit_rate_limit = get_intraday_data(access_token, email_address, date_str, last_synch_date)
 
-            current_date = current_date + timedelta(days=1)
+    #         current_date = current_date + timedelta(days=1)
